584991bdcc969da196d8b5556f67648da118e334
mcanat <mcanat@ku.edu.tr>
2016-10-02 02:55:50 +0200

    Added foo to the bar

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  osmandeneme
+//
+//  Created by Mert Canat on 01/10/2016.
+//  Copyright © 2016 Mert Canat. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
